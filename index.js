2fc2a1a8fae88383741c47a0e91584343003786f
Paulo <pmarsiglia93@gmail.com>
2022-04-13 21:48:25 -0300

    @pmarsiglia93 [TASK:Atributos Privados]

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 //Classe é o molde.
 //cliente1 e cliente2 são os meus objetos.
 //cliente1 é uma instancia da minha classe.
+
+//Proposta de campos privados
+// _saldo valor privado.
 class Cliente {
     nome;
     cpf;  
@@ -9,14 +12,21 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
     //Método
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
         }else{
             console.log("valor insuficiente para saque!")
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor;
+            console.log(this._saldo);
         }
     }
 }
@@ -31,15 +41,12 @@ cliente2.cpf = "888.222.333-09";
 
 const contaCorrenteRicardo = new ContaCorrente();
 
-contaCorrenteRicardo.saldo = "0";
-contaCorrenteRicardo.agencia = "1001";
+contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = "100";
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(30);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
 
-
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
